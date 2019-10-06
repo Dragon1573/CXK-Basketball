9b30b39e15a48c8d30a5e4d35918010a4ddc49de
tiancaits <56194187+tiancaits@users.noreply.github.com>
2019-10-06 17:44:00 +0800

    Update geme.js

--- js/geme.js
+++ js/geme.js
@@ -1,6 +1,4 @@
-/* by：弦云孤赫——David Yang
-** github - https://github.com/yangyunhe369
-*/
+
 // 创建储存积分的变量
 var storageScore = 0;
 // 创建储存总分的变量
@@ -81,7 +79,7 @@ class Game {
     // 绘制提示文字
     this.context.font = '32px Microsoft YaHei'
     this.context.fillStyle = '#000'
-    this.context.fillText('CXK，你球掉了！得分：' + globalScore, 404, 226)
+    this.context.fillText('你没有接住坤坤的篮球！得分：' + globalScore, 404, 226)
 	$("#ballspeedset").removeAttr("disabled");
 	// audio.pause();
 	globalScore = 0;
